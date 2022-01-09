45b61f702ec409f8165297859b59c49c572df907
turnkevin <lushikai277@gmail.com>
2022-01-09 18:52:38 +1100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//////////////
+//Sticky Navigation
+const sectionMainEl = document.querySelector(".section-mainpage");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionMainEl);
+
+////////////////////////
